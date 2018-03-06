58ec8d2e1d7f18fd7e47dd93804426be712d2984
SuuperW <SuuperW@gmail.com>
2018-03-06 09:45:56 -0600

    Add IMemoryHacker64Bit.cs

---
+++ DeSmuME_Watch/MemoryHackers/IMemoryHacker64Bit.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DeSmuME_Watch
+{
+    public interface IMemoryHacker64Bit : IDisposable
+    {
+        byte[] ReadBytes(long MemoryAddress, int Count);
+        void WriteBytes(long MemoryAddress, byte[] val);
+
+        byte ReadByte(long MemoryAddress);
+        void WriteByte(long MemoryAddress, byte val);
+
+        short ReadShort(long MemoryAddress);
+        void WriteShort(long MemoryAddress, short val);
+
+        int ReadInteger(long MemoryAddress);
+        void WriteInteger(long MemoryAddress, int val);
+        uint ReadUInteger(long MemoryAddress);
+        void WriteUInteger(long MemoryAddress, uint val);
+
+        event Action Updated;
+    }
+}
